b306c1d6371f1f4df9e96eefeb23ac4d70ccee48
Cote <majovardon@gmail.com>
2018-07-20 00:02:37 -0400

    Cambios en el CSS

--- src/js/appmuro.js
+++ src/js/appmuro.js
@@ -14,13 +14,15 @@ firebase.database().ref('messages')
     const messageContainer = document.createElement('div')
     wallMessages.appendChild(messageContainer)
     messageContainer.innerHTML +=
-      `<div style = "background-color: #E8910C" class="mb-3">
-        <p class="m-0">Autor: ${newMessage.val().creatorName}</p>
-        <p class="m-0" dataKeyEdit="${newMessage.key}">Mensaje<br>${newMessage.val().text}</p>
-          <i class="fas fa-star p-1 pb-1" dataKey="${newMessage.key}" onclick="starPost(event)"></i><span>${newMessage.val().stars}</span>
-          <i class="far fa-edit p-1 pb-1" onclick="editPost(event)">
-          </i>
-          <i class="fas fa-trash-alt p-1 pb-1" dataKey="${newMessage.key}" onclick="deletePost(event)"></i>
+      `<div class="container">
+        <div style = "background-color: #E8910C" class="mb-3">
+          <p class="m-0">Autor: ${newMessage.val().creatorName}</p>
+          <p class="m-0" dataKeyEdit="${newMessage.key}">Mensaje<br>${newMessage.val().text}</p>
+            <i class="fas fa-star p-1 pb-1" dataKey="${newMessage.key}" onclick="starPost(event)"></i><span>${newMessage.val().stars}</span>
+            <i class="far fa-edit p-1 pb-1" onclick="editPost(event)">
+            </i>
+            <i class="fas fa-trash-alt p-1 pb-1" dataKey="${newMessage.key}" onclick="deletePost(event)"></i>
+        </div>
       </div>`;
   });
 
